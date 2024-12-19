f2c9b8442638d9bf50a6cf153d7884ae37e99e8f
zain shekhani <157803688+zainshekhani54321@users.noreply.github.com>
2024-12-19 20:24:14 +0500

    Add files via upload

---
+++ stop watch/index.js
@@ -0,0 +1,60 @@
+
+// var minutes = 0
+// var second = 0
+// var milisecond = 0
+
+// var getmin = document.getElementById('min')
+// var getsec = document.getElementById('sec')
+// var getmsec = document.getElementById('msec')
+// var interval;
+// var color = ["red", "green","yellow"]
+
+
+
+//       function start(){
+//    interval =    setInterval(function(){
+
+//         milisecond++
+//         getmsec.innerHTML=milisecond
+    
+//         if(milisecond >= 100){
+//             second++
+//             getsec.innerHTML=second
+//             milisecond = 0
+            
+//     for(var i= 0 ; i< 1 ; i++){
+//         var ab = Math.floor(Math.random()* color.length )
+//         document.body.style.backgroundColor = color[ab]
+//     }
+    
+//         }
+//         else if(second >= 5){
+//             minutes++
+//             getmin.innerHTML=minutes
+//             second = 0
+//         }
+    
+    
+    
+//     },10)
+
+//     document.getElementById('ss').disabled = true
+// }
+
+
+//  function stop(){
+//     clearInterval(interval)
+    
+//     document.getElementById('ss').disabled = false
+//  }
+
+//  function reset(){
+//     minutes = 0
+//     second = 0
+//     milisecond = 0
+
+//     getmin.innerHTML=minutes
+//     getsec.innerHTML=second
+//     getmsec.innerHTML=milisecond
+//     document.getElementById('ss').disabled = false
+//  }
